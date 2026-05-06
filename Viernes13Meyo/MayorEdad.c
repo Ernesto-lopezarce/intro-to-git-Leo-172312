e396d299e54efb8cad2a8901717abf22a1816761
Leo-172312 <leonardo.garcia3203@alumnos.udg.mx>
2026-05-06 08:24:25 -0600

    tarea-leonardogarcia

--- Viernes13Meyo/MayorEdad.c
+++ Viernes13Meyo/MayorEdad.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main() {
+    int edad;
+    printf("Indique su edad: ");
+    scanf("%d", &edad);
+
+    if(edad >= 18){
+        printf("Eres mayor de edad\n");
+    }
+    else{
+        printf("Te falta vivir bb\n");
+    }
+
+    return 0;
+}
